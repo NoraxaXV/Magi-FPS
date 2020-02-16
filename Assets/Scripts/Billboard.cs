8683777ccb8081d4214811461b0a45812c2c32a1
Marcus Smith <arabellekids@gmail.com>
2020-02-16 18:21:47 -0500

    Added TMP

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    Transform cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(cam);
+    }
+}
